1323f87755d1a455664c1409b5d9e9e53460035b
Naman Patel <62150420+namannpatel@users.noreply.github.com>
2024-08-15 19:14:53 +0530

    Add files via upload

---
+++ week1/Assignment1/circle.cpp
@@ -0,0 +1,9 @@
+// Write a program to find the circumference of a circle with radius 10
+
+#include<iostream>
+using namespace std;
+int main(){
+    float radius = 10;
+    float area = radius * radius * 3.14;
+    cout<<area;
+}
