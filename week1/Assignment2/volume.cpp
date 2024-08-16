da2903b31677b37c70a1e9f178acf48b58fe0ac4
Naman Patel <62150420+namannpatel@users.noreply.github.com>
2024-08-16 18:28:52 +0530

    Add files via upload

---
+++ week1/Assignment2/volume.cpp
@@ -0,0 +1,13 @@
+// WAP for finding the volume of the cylinder by taking radius and height as input
+
+#include<iostream>
+using namespace std;
+int main(){
+    float r, h;
+    cout<<"Enter radius of cylinder : ";
+    cin>>r;
+    cout<<"Enter height of cylinder : ";
+    cin>>h;
+    float vol = 3.14 * r * r * h;
+    cout<<"Volume of cylinder : "<<vol;
+}
